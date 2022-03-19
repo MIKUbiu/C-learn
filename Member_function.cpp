b5c4fd56a6e91fe29577dd50b0f8ec742f96555c
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-19 15:48:16 +0800

    Add files via upload

---
+++ Member_function.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+class Box {
+public:
+	double length, breadth, height;
+	double Volume();
+
+};
+double Box::Volume()
+{
+	return length * breadth * height;
+}
+int main() {
+	Box b1;
+	double volume;
+	cin >> b1.length >> b1.breadth >> b1.height;
+	volume = b1.Volume();
+	cout << volume;
+	return 0;
+}
